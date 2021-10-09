ff58552c7aa5c8ed35c5ced48745c0a967b55ab9
Muhammad Zain <Zainsaleem456@gmail.com>
2021-10-09 20:09:31 +0500

    Quiz Added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
 import Swal from "sweetalert2";
-import reactImg from "../../assets/ReactJs.png"
+import reactImg from "../../assets/ReactJs.png";
 import {
   InputGroup,
   InputGroupAddon,
@@ -19,28 +19,98 @@ import {
   CardSubtitle,
 } from "reactstrap";
 // import { useHistory } from "react-router-dom";
-import { useState } from "react";
+import { useEffect,useState } from "react";
 
-function index() {
+const Index = () => {
+  const [data, setdata] = useState([]);
+  const [mainQuiz, setmainQuiz] = useState("");
+  
+  useEffect(() => {
+    let get = JSON.parse(localStorage.getItem("Quiz"));
+    setdata(get);
+  },[]);
+  
+  const valueGet = () => {
+    
+  }
+  const createquiz = () => {
+    if (mainQuiz == "") {
+      alert("Please Add Quiz")
+      return false;
+    }
+    let obj = {
+      id: Math.floor(Math.random() * 100),
+      quiz: mainQuiz,
+    };
+    // console.log(obj);
+    let get = JSON.parse(localStorage.getItem("Quiz"));
+    let main = true;
+
+    if (get && get.length) {
+      for (let i = 0; i < get.length; i++) {
+        if (get[i].quiz == obj.quiz) {
+          main = false;
+          console.log(obj);
+          alert("Already exist");
+          break;
+        }
+      }
+      if (main) {
+        let dupdata = [...data];
+        dupdata.push(obj);
+        setdata(dupdata);
+        let ls = localStorage.setItem("Quiz", JSON.stringify(dupdata));
+        //   console.log("True data");
+      }
+    } else {
+      let ls = localStorage.setItem("Quiz", JSON.stringify([obj]));
+    }
+    // console.log(main);
+  };
+  // console.log(data);
+
   return (
     <Container>
-     <div>
-         <Row>
-<Col md={4}>
-      <Card>
-        <CardImg top width="100%" src={reactImg} alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">REACT</CardTitle>
-         
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button color="outline-success">Show Quiz</Button>
-        </CardBody>
-      </Card>
-</Col>
-         </Row>
-    </div>
+      <Row>
+        <Col md={4}>
+          <Input
+            onChange={(event) => setmainQuiz(event.target.value)}
+            value={mainQuiz}
+            placeholder="Create Quiz"
+          />
+        </Col>
+
+        <Col md={4}>
+          <Button color="outline-primary" onClick={createquiz}>
+            Create Quiz
+          </Button>
+        </Col>
+      </Row>
+      <div>
+        <Row className="mt-4">
+
+          {data.map((item, index) => {
+            return(
+            <Col md={4} key={index}>
+            <Card >
+              <CardImg top width="100%" src={reactImg} alt="Card image cap" />
+              <CardBody>
+                <CardTitle tag="h5" value={item.quiz}>{item.quiz}</CardTitle>
+
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </CardText>
+                <Button color="outline-success" >Show Quiz</Button>
+              </CardBody>
+            </Card>
+          </Col>
+            )
+          })}
+        </Row>
+      </div>
     </Container>
   );
-}
+};
 
-export default index;
+export default Index;
